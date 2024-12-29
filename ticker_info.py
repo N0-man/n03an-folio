2ebc4845a99a97ae032dc5b97fba74b0b3031b6c
n03an <nouman.memon@icloud.com>
2024-12-29 19:51:06 +0800

    fix styling for differentn resolution

---
+++ ticker_info.py
@@ -0,0 +1,15 @@
+PLL = """
+Piedmont Lithium is a lithium mining and processing company focused on developing lithium resources primarily in North America. Their main revenue currently comes from their '25%' ownership in the North American Lithium (NAL) operation in Quebec, where they receive a portion of spodumene concentrate production. They are also developing several other projects including Carolina Lithium (US), and have a joint venture interest in the Ewoyaa project in Ghana.
+
+### Acquisition
+
+- Sayona Mining announced it will merge with Piedmont Lithium to create a global lithium leader, with Sayona as the parent entity. The deal is expected to close after shareholder approval in March of 2025.
+- As part of the merger, the parties intend to raise equity capital aggregating to approximately US$99 million.
+- Piedmont intends to raise ~$27 million. This is why we think the stock is down today, i.e. expected shareholder dilution.
+- Otherwise, the terms of the merger provide that 527 shares of Sayona will be exchanged for each 1 Piedmont share.
+- Currently Sayona is trading at 0.025. So at today's prices, 1 share of Piedmont should equate to: 527.025 = $13.175
+- The current market price of PLL is $10.80 so we're about '20%' discounted from what the merger terms suggest the price of PLL should be.
+- Importantly, we assess the merger itself to be a long term positive for current PLL shareholders, here's why:
+- The resulting company will become the biggest hard rock lithium producer in North America, with substantial reserves and resources. As such, it should benefit from operational efficiencies with Lower costs, faster permitting, significantly expanded customer relationships  and streamlined logistics and procurement, leading to reduced operating expenses.
+
+"""
